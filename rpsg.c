6e83f9483fc93da38b0548e274887a67c5e02792
ThatGuyCodes <jamielkylelan@outlook.com>
2025-11-23 17:14:13 +0000

    Add files via upload

--- rpsg.c
+++ rpsg.c
@@ -12,32 +12,39 @@ int main(void) {
     srand(time(NULL));
     clrscr();
     printf("*** ROCK PAPER SCISSORS ***\n");
-    usrchoice = getusrchoice();
-    pcchoice = getpcchoice();
-    switch (usrchoice) {
-        case 1:
-            printf("You chose ROCK\n");
-            break;
-        case 2:
-            printf("You chose PAPER\n");
-            break;
-        case 3:
-            printf("you chose SCISSORS\n");
-            break;
-    }
-    switch (pcchoice) {
-        case 1:
-            printf("CPU chose ROCK\n");
-            break;
-        case 2:
-            printf("CPU chose PAPER\n");
-            break;
-        case 3:
-            printf("CPU chose SCISSORS\n");
-            break;
+ 
+
+    do{
+        usrchoice = getusrchoice();
+        pcchoice = getpcchoice();
+        switch (usrchoice) {
+            case 1:
+                printf("You chose ROCK\n");
+                break;
+            case 2:
+                printf("You chose PAPER\n");
+                break;
+            case 3:
+                printf("you chose SCISSORS\n");
+                break;
+        }
+        switch (pcchoice) {
+            case 1:
+                printf("CPU chose ROCK\n");
+                break;
+            case 2:
+                printf("CPU chose PAPER\n");
+                break;
+            case 3:
+                printf("CPU chose SCISSORS\n");
+                break;
+            case 4:
+                break;
+        
     }
     checkwinner(usrchoice, pcchoice);
-    cgetc();
+    }while (usrchoice != 4);
+    
     return 0;
 }
 int getpcchoice() {
@@ -48,17 +55,17 @@ int getusrchoice(void) {
     char c;
     do {
         printf("choose an option\n");
-        printf("1.rock\n2.paper\n3.scissors\n");
+        printf("1.rock\n2.paper\n3.scissors\n4. exit\n");
         printf("enter your choice: ");
         c = cgetc();      // Use cgetc() instead of scanf
         printf("%c\n", c);
         choice = c - '0'; // Convert char to int
-    } while (choice < 1 || choice > 3);
+    } while (choice < 1 || choice > 4);
     return choice;
 }
 void checkwinner(int usrchoice, int pcchoice) {
     if (usrchoice == pcchoice) {
-        printf("iTS A TIE!\n");
+        printf("ITS A TIE!\n");
     }
     else if (usrchoice == 1 && pcchoice == 3) {
         printf("YOU WIN!\n");
